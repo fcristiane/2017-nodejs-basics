d8a35888ffda406343d1e618687bea3686380018
Cristiane Ferreira <cristianedeferreira@gmail.com>
2017-09-14 21:17:35 -0300

    resolve exercise report

---
+++ 02-modules/report.js
@@ -0,0 +1,18 @@
+const students = require('./students.json')
+
+// var male=0
+// var female=0
+// for (var i = 0; i  < students.length;i++) {
+//          var student = students[i];
+//          console.log("Name: "+student.name)
+//          console.log("Genre: "+student.genre)
+//          if (student.genre == 'M') {
+//             male++
+//         }else{
+//             female++
+//         }
+        
+// }
+
+// console.log('Woman Students: '+ female)
+// console.log('Men Students: '+male)
